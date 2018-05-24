0dcb38cedbe3ec9a67eb896da3d7ad57e1f6db94
salimbencosme <salimbencosme@gmail.com>
2018-05-23 21:55:17 -0400

    First commit

---
+++ android/src/com/gamestudio24/martianrun/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gamestudio24.martianrun;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gamestudio24.martianrun.MartianRun;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MartianRun(), config);
+	}
+}
---
+++ desktop/src/com/gamestudio24/martianrun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gamestudio24.martianrun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gamestudio24.martianrun.MartianRun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MartianRun(), config);
+	}
+}
