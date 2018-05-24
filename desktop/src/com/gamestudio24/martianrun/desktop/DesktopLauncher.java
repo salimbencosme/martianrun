8ebf59b542a8dd4117d6ed09216ddeafeba0251e
salimbencosme <salimbencosme@gmail.com>
2018-05-23 22:54:14 -0400

    First tutorial--- need to run correctly

--- desktop/src/com/gamestudio24/martianrun/desktop/DesktopLauncher.java
+++ desktop/src/com/gamestudio24/martianrun/desktop/DesktopLauncher.java
@@ -3,10 +3,13 @@ package com.gamestudio24.martianrun.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gamestudio24.martianrun.MartianRun;
+import com.gamestudio24.martianrun.utils.Constants;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Constants.APP_WIDTH;
+		config.height = Constants.APP_HEIGHT;
 		new LwjglApplication(new MartianRun(), config);
 	}
 }
